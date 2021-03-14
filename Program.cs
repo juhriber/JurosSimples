a4fe1028c9f0037b8ac5c3f928a358adf978e1ba
juhriber <j.de.s.r.25@gmail.com>
2021-03-14 18:08:44 -0300

    Finalização do projeto JurosSimples

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capital;
+            double taxa;
+            double resultTaxa;
+            double tempo;
+            double juros;
+            double montante;
+
+            Console.Write("Digite seu Valor R$: ");
+            capital = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua Taxa de Juros %: ");
+            taxa = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu Tempo de Parcelas: ");
+            tempo = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            resultTaxa = taxa / 100;
+            juros = capital * resultTaxa * tempo;
+            Console.WriteLine("Seus Juros é = {0}", juros);
+
+            montante = capital + juros;
+            Console.WriteLine("Seu Montante é = {0}", montante);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
